9c8bbbdb31cf1ae656b0b4be3157a3eebec73464
Mikey Pioli <gowermikey@gmail.com>
2023-11-02 18:37:23 -0400

    fix deployment bug

--- src/components/Main/Home/Home.tsx
+++ src/components/Main/Home/Home.tsx
@@ -9,7 +9,7 @@ function Home() {
   const position = useScrollPosition((position) => Math.ceil((100 * position) / window.innerHeight))
   return (
     <div className={styles.container}>
-      <div className={styles.image} style={createCssVariable('pos', `${position}%`)}>
+      <div className={styles.image} style={createCssVariable(['pos', `${position}%`])}>
         <div />
         <h5>celebrate what makes you</h5>
         <img src={uniquelyBeautiful} alt='uniquely beautiful' />
