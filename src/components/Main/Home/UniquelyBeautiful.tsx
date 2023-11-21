743ea38280bd7e6beea4fb043da6a3aefb6e6ba8
Mikey Pioli <gowermikey@gmail.com>
2023-11-21 13:18:33 -0500

    fix error in build

--- src/components/Main/Home/UniquelyBeautiful.tsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-interface Props {
-  //
-}
-
-export default function UniquelyBeautiful(props: Props) {
-  return <div></div>
-}
