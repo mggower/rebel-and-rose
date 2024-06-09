e02d8d33653d3fd9dffb01ecb35836d41656c632
Mikey Pioli <gowermikey@gmail.com>
2024-06-09 16:40:40 -0600

    update color palette with more range and saturation

--- rebel-and-rose/theme/colors.ts
+++ rebel-and-rose/theme/colors.ts
@@ -1,71 +1,83 @@
 const WHEAT = {
-  100: '#FAF8F0',
-  200: '#F5EFDE',
-  300: '#F0E4CC',
-  400: '#E9D9BC',
-  500: '#D9C29E',
-  600: '#C8A881',
-  700: '#B78F6A',
-  800: '#AA7751',
+  50: '#FDF7EF',
+  100: '#FDEFDB',
+  200: '#F9E3C4',
+  300: '#F3CFA4',
+  400: '#EBBB85',
+  500: '#DEA465',
+  600: '#CB8845',
+  700: '#AB6422',
+  800: '#874609',
 } as const
 
 const BLUSH = {
-  100: '#FDDDCF',
-  200: '#F0C0AC',
-  300: '#E8A88C',
-  400: '#E08668',
-  500: '#D6644C',
-  600: '#C24D3B',
-  700: '#AA382B',
-  800: '#862118',
+  100: '#FEE7E1',
+  200: '#FBCEC3',
+  300: '#FBBAA9',
+  400: '#F49C8A',
+  500: '#E67667',
+  600: '#D8473E',
+  700: '#C32C27',
+  800: '#AB0E0E',
 } as const
 
-const RUSSET = {
-  100: '#EFC29C',
-  200: '#DFA778',
-  300: '#CF8A50',
-  400: '#B67035',
-  500: '#A15F27',
-  600: '#874E1E',
-  700: '#673A13',
-  800: '#4D2909',
+const BLONDE = {
+  100: '#FFE5C2',
+  200: '#FFCF8F',
+  300: '#FBB860',
+  400: '#F5A145',
+  500: '#EA8A34',
+  600: '#E37A1E',
+  700: '#D66A0B',
+  800: '#C55D01',
 } as const
 
 const EARTH = {
-  100: '#E9E9E2',
-  200: '#D5D7BF',
-  300: '#B4B892',
-  400: '#989E72',
-  500: '#7D8255',
-  600: '#575B38',
-  700: '#46482C',
-  800: '#34351D',
+  100: '#CDD9CE',
+  200: '#A8BFAB',
+  300: '#839F86',
+  400: '#647D67',
+  500: '#4A5F4D',
+  600: '#354737',
+  700: '#243225',
+  800: '#141F15',
+} as const
+
+const LAVENDER = {
+  100: '#F1E1EE',
+  200: '#EBCEE7',
+  300: '#DDB8D7',
+  400: '#C497BD',
+  500: '#B67FAD',
+  600: '#A66D9D',
+  700: '#96588C',
+  800: '#7F3874',
 } as const
 
 const INK = {
   50: '#FFFFFF',
-  75: '#F4F4F4',
-  100: '#E8E8E8',
-  200: '#D2D2D2',
-  300: '#B3B3B3',
-  400: '#949292',
-  500: '#7B7979',
-  600: '#646262',
-  700: '#545252',
-  800: '#414040',
-  900: '#302F2F',
-  950: '#1B1B1B',
+  100: '#F1F1F1',
+  200: '#E0E0E0',
+  300: '#CBCBCA',
+  400: '#AAAAA8',
+  500: '#838380',
+  600: '#63635E',
+  700: '#4A4C43',
+  800: '#313328',
+  900: '#1D1F13',
 } as const
 
 const TRANSPARENT = 'transparent'
 
 const TOKEN = {
   transparent: TRANSPARENT,
-  wheat: WHEAT[400],
+  wheat: WHEAT[300],
   blush: BLUSH[300],
-  russet: RUSSET[500],
-  earth: EARTH[700],
-  ink: INK[950],
+  russet: WHEAT[700],
+  earth: EARTH[500],
+  blonde: BLONDE[400],
+  lavender: LAVENDER[200],
+  ink: INK[900],
   white: INK[50],
 }
 
@@ -73,8 +85,9 @@ const colors = {
   token: TOKEN,
   wheat: WHEAT,
   blush: BLUSH,
-  russet: RUSSET,
+  blonde: BLONDE,
   earth: EARTH,
+  lavender: LAVENDER,
   ink: INK,
 }
 
