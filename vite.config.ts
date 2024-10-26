0ce8eddbd24aad181213887f51699440d778a3d0
Mikey Pioli <gowermikey@gmail.com>
2024-10-26 11:43:31 -0600

    use image optimizer plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react()],
+  plugins: [svgr(), react(), ViteImageOptimizer()],
   css: { modules: { localsConvention: 'camelCase' } },
   resolve: { alias: [{ find: /^@\/(.*)/, replacement: '/src/$1' }] },
 })
